4e51b36d515b35d25647dbd04c05563cada07fe2
nazwaasfiani <110007985+nazwaasfiani@users.noreply.github.com>
2022-08-09 11:58:23 +0700

    Add files via upload

---
+++ tambah2.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form method="get" action="proses_tambah2.php">
+     <p> Name <input name="nama"  type="text"></p>
+     <p> kelas<input name="kelas" type="text"></p>
+     <button type="submit">simpan</button>
+</form>
+</body>
+</html>
